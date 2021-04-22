3035cb7ecd3400502c549fd995facf1285352fd3
ColtxZZ <82120154+ColtxZZ@users.noreply.github.com>
2021-04-22 16:13:44 -0300

    Update GetOrderDao.java

--- src/main/java/com/pluralsight/order/dao/GetOrderDao.java
+++ src/main/java/com/pluralsight/order/dao/GetOrderDao.java
@@ -40,7 +40,7 @@ public class GetOrderDao {
             if(rs.next()) {
                 orderDto = new OrderDto();
                 orderDto.setOrderId(rs.getLong("order_id"));
-                orderDto.setCustomerId(rs.getLong("order_costumer_id"));
+                orderDto.setCustomerId(rs.getLong("order_customer_id"));
                 orderDto.setDate(rs.getTimestamp("order_date"));
                 orderDto.setStatus(rs.getString("order_status"));
             }
